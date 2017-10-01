620d9d998323a2afd3485eaf377b97455c43a261
randy-r <31799713+randy-r@users.noreply.github.com>
2017-10-01 21:25:33 +0300

    Basic grid.

--- src/components/ReduxSnake.jsx
+++ src/components/ReduxSnake.jsx
@@ -1,4 +1,32 @@
 import React from 'react';
+// import styles from './styles.css';
+
+const gridSize = 7;
+
+
+const styles = {
+  wrap: {
+    width: '80%',
+    margin: '0 auto',
+  },
+  wrapDiv: {
+    width: `${100 / gridSize}%`,
+    paddingBottom: `${100 / gridSize}%`,
+    boxBizing: 'border-box',
+    MozBoxSizing: 'border-box',
+    WebkitBoxSizing: 'border-box',
+    // margin: '1%',
+    borderStyle: 'solid',
+    borderColor: '#ff0000',
+    float: 'left',
+    background: 'darkblue',
+    // borderWidth: '10px',
+  },
+  br: {
+    clear: 'both',
+  },
+};
+
 
 
 class ReduxSnake extends React.Component {
@@ -10,10 +38,27 @@ class ReduxSnake extends React.Component {
     }, 800);
   }
 
-  render() {
+  buildGrid = (size) => {
+    const rows = [];
+    for (let i = 0; i < size; i += 1) {
+      const elements = [];
+      for (let j = 0; j < size; j += 1) {
+        elements.push(<div key={j} style={styles.wrapDiv} />);
+      }
+      const row = (<div key={i} >{elements}</div>);
+      rows.push(row);
+      rows.push(<br key={size + i} style={styles.br} />);
+    }
+
     return (
-      <div>{this.props.text}</div>
+      <div style={styles.wrap} >
+        {rows}
+      </div >
     );
+  }
+
+  render() {
+    return this.buildGrid(gridSize);
   }
 }
 
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -6,6 +6,7 @@ module.exports = {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, loader: 'style-loader!css-loader', exclude: /node_modules/ },
     ],
   },
 };
